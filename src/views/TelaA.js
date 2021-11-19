7ab642446b310ca50172950dd14e43d187a39ba0
Joao Ferigato <joaoferigato@gmail.com>
2021-11-19 14:09:55 -0300

    working with navigation stack

--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
+import PassoStack from '../components/PassoStack'
 
-export default props => {
-    return (
-        <TextoCentral corFundo='#e53935'>
-            Tela A
-        </TextoCentral>
-    )
-}
+export default props => (
+    <TextoCentral corFundo='#e53935'>
+        Tela A
+    </TextoCentral>
+)
